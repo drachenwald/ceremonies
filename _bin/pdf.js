2c215797fd33cd5b7853cd2caa98d9388b8359b3
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2023-04-21 22:59:01 +0100

    is this how puppeteer works

--- _bin/pdf.js
+++ _bin/pdf.js
@@ -35,21 +35,28 @@ async function pdfify() {
               browser.close();
             })
 
-    await page
+    const page2 = await browser.newPage()
+
+    await page2
+        .on('console', message => console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+        .on('pageerror', ({ message }) => console.log(message))
+        .on('response', response => console.log(`${response.status()} ${response.url()}`))
+        .on('requestfailed', request => console.log(`${request.failure().errorText} ${request.url()}`))
+
+    await page2
         .goto(`http://localhost:3000/largeprint/`, { waitUntil: ['domcontentloaded', 'networkidle0', 'load'] })
 
     console.log('waiting a little longer')
 
     await new Promise(resolve => setTimeout(resolve, 10000));
 
-    await page.pdf({ path: '_site/ceremonies-largeprint.pdf', format: 'A4' })
+    await page2.pdf({ path: '_site/ceremonies-largeprint.pdf', format: 'A4' })
             .then(function (res) {
               browser.close();
             }).catch(function (e) {
               browser.close();
             })
     
-
     console.log('success!')
     server.close(() => {
       console.log('HTTP server closed')
