12fcccf9786280b3b1919ede77a1548f4ee51dc6
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2023-04-21 23:51:42 +0100

    commit hash and build date

--- _bin/build.sh
+++ _bin/build.sh
@@ -1,3 +1,9 @@
+echo -n 'commit: ' >> _data/meta.yaml
+echo $CF_PAGES_COMMIT_SHA >> _data/meta.yaml
+echo -n 'buildtime: "' >> _data/meta.yaml
+echo -n `date --rfc-3339="seconds"` >> _data/meta.yaml
+echo '"' >> _data/meta.yaml
+
 bundle exec jekyll build
 node _bin/pdf.js
 node _bin/pdf-largeprint.js
