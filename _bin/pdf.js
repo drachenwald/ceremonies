2cbb6da4ff99b06ad65f74f11c7e12e66293b6c9
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2023-05-10 14:50:16 -0400

    moved jekyll output

--- _bin/pdf.js
+++ _bin/pdf.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('_site'))
+app.use(express.static('public'))
 
 const server = app.listen(port, () => {
   pdfify()
