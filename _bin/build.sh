fa20bf9810980729f137d53de1605b5dacee33b1
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2023-05-10 14:07:31 -0400

    publish with github actions

--- _bin/build.sh
+++ _bin/build.sh
@@ -1,5 +1,5 @@
 echo -n 'commit: ' >> _data/meta.yaml
-echo $CF_PAGES_COMMIT_SHA >> _data/meta.yaml
+echo $GITHUB_SHA >> _data/meta.yaml
 echo -n 'buildtime: "' >> _data/meta.yaml
 echo -n `date --rfc-3339="seconds"` >> _data/meta.yaml
 echo '"' >> _data/meta.yaml
---
+++ _bin/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+eval $(ssh-agent -s)
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+echo "${OVH_PRIVATE}" | tr -d '\r' | ssh-add - > /dev/null
+
+if [ -n $GITHUB_REF_NAME ]
+then
+  if [ "$GITHUB_REF_NAME" == "main" ]
+  then
+    echo "Building main site"
+    JEKYLL_ENV=production bundle exec jekyll build -d public
+    rsync -rvzc --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:ceremonies/public/
+  else
+    echo "No branch specified"
+  fi
+else
+  echo "No branch specified"
+fi
