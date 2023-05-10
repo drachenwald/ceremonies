b6b2c615412b5c421728bcc307c9079b7ce5d6b2
anna-wilson <30553780+anna-wilson@users.noreply.github.com>
2023-05-10 17:14:28 -0400

    cache npm deps

--- _bin/build.sh
+++ _bin/build.sh
@@ -5,6 +5,5 @@ echo -n `date --rfc-3339="seconds"` >> _data/meta.yaml
 echo '"' >> _data/meta.yaml
 
 JEKYLL_ENV=production bundle exec jekyll build -d public
-npm install
 node _bin/pdf.js
 node _bin/pdf-largeprint.js
